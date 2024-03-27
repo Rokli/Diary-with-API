7799e320d3eabe0ddc2ae5918a1930922bfefd88
hikiray <satalomaksimka12345678910@gmail.com>
2024-03-27 19:45:04 +0700

    Connected Bootstrap

--- Calendar/resources/views/diary.blade.php
+++ Calendar/resources/views/diary.blade.php
@@ -6,7 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ежедневник</title>
-    @vite(['resources/css/diary.css','resources/js/diary.js'])
+    @vite(['resources/css/diary.css','resources/js/diary.js',
+            'resources/js/bootstrap.js','resources/js/app.js',
+            'node_modules/bootstrap/scss/bootstrap.scss'])
 </head>
 <body>
 <h1>Ежедневник</h1>
@@ -148,6 +150,22 @@
     </div>
 </div>
 
+<form>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">Мы никогда никому не передадим вашу электронную почту.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Пароль</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Проверить меня</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Отправить</button>
+</form>
 
 </body>
 </html>
--- Calendar/vite.config.js
+++ Calendar/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/diary.css', 'resources/js/diary.js'],
+            input: ['resources/css/diary.css', 'resources/js/diary.js', 'resources/js/bootstrap.js','resources/js/app.js','node_modules/bootstrap/scss/bootstrap.scss'],
             refresh: true,
         }),
     ],
